ae64d0a6948c0d49fc551786444799e17927fc4f
rickyhemawan <rickyhemawan23@gmail.com>
2021-03-05 07:14:00 +0800

    done for anime

--- helpers/anime-fetcher.js
+++ helpers/anime-fetcher.js
@@ -15,10 +15,21 @@ const seasonalAnime = async () => {
     // or limit from the 3rd API
     return anime
       .sort((a, b) => b.score - a.score)
+      .filter((e) => e.episodes)
       .slice(0, 10)
-      .map((e) => {
-        const { url, title, image_url, synopsis, score } = e
-        return { url, title, image_url, synopsis, score }
+      .map((e, index) => {
+        console.log(e)
+        const { mal_id, url, title, image_url, synopsis, score, episodes } = e
+        return {
+          mal_id,
+          url,
+          title,
+          image_url,
+          synopsis,
+          score,
+          episodes,
+          index,
+        }
       })
   } catch (error) {
     console.error(error)
