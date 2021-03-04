4c57f26a8eb0b8c946e886bf9d67aab363ff85c2
rickyhemawan <rickyhemawan23@gmail.com>
2021-03-04 19:39:29 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const router = require('./routes')
+const errorHandler = require('./middlewares/error-handler')
+const PORT = 3000
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', router)
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log('Listing at PORT', PORT))
