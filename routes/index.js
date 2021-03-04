6b5fd7249a42ecab4d54a2d235b9798d0c1ddd5c
haritsmedina <haritsmedina@gmail.com>
2021-03-05 03:12:05 +0700

    oAuth

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const router = require('express').Router()
-const { UserController } = require('../controllers')
+const { UserController,OauthController } = require('../controllers')
+
 
 const animeRouter = require('./anime')
+const oauthRouter = require('./oAuth')
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 
 router.use('/anime', animeRouter)
+router.use('/',oauthRouter)
 
 module.exports = router
